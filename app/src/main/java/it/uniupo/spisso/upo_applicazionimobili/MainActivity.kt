d50884c789929d3d1ab5dabec029f0f41738c453
Alessandro Spisso <alex@outlook.it>
2020-01-06 00:02:50 +0100

    First commit empty app + gitignore updated

---
+++ app/src/main/java/it/uniupo/spisso/upo_applicazionimobili/MainActivity.kt
@@ -0,0 +1,12 @@
+package it.uniupo.spisso.upo_applicazionimobili
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
