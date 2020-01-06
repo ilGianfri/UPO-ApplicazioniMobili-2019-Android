5890bdebd06ed7d1b376896367464ae666fe83ea
Alessandro Spisso <alex@outlook.it>
2020-01-06 14:20:34 +0100

    Added login activity and day/night mode instant switching

---
+++ app/src/main/java/it/uniupo/spisso/upo_applicazionimobili/LoginActivity.kt
@@ -0,0 +1,21 @@
+package it.uniupo.spisso.upo_applicazionimobili
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        //Tells the activity to use DayNight theme
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        //to change title of activity
+        val actionBar = supportActionBar
+        actionBar!!.title = getString(R.string.login_title)
+    }
+}
